308c422d39d29b046d3991b3628b55b81385c6c2
Antoni Gzara <antek.gzara@gmail.com>
2021-09-21 22:28:25 +0200

    added files

---
+++ mouse/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace mouse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mouse";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(344, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Keyboard";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::mouse.Properties.Resources.on;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 134);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::mouse.Properties.Resources.on;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(285, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(209, 134);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::mouse.Properties.Resources.ckickSound;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Location = new System.Drawing.Point(198, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(108, 100);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(506, 242);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MouseClick Sound";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ mouse/Form1.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+using System.Diagnostics;
+
+namespace mouse
+{
+    public partial class Form1 : Form
+    {
+        MouseHook mh;
+        LowLevelKeyboardListener listener;
+        private bool clicked = true;
+
+        private bool KeyboardState = true;
+        private bool MouseState = true;
+
+        private bool KeyboardSoundState = false;
+
+        System.Media.SoundPlayer MouseSound = new System.Media.SoundPlayer(Properties.Resources.click2);
+        System.Media.SoundPlayer KeyboardSound1 = new System.Media.SoundPlayer(Properties.Resources.KeyClick);
+        System.Media.SoundPlayer KeyboardSound2 = new System.Media.SoundPlayer(Properties.Resources.KeyClick);
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            mh = new MouseHook();
+            mh.SetHook();
+            mh.MouseMoveEvent += mh_MouseMoveEvent;
+            mh.MouseClickEvent += mh_MouseClickEvent;
+            mh.MouseDownEvent += mh_MouseDownEvent;
+            mh.MouseUpEvent += mh_MouseUpEvent;
+
+            listener = new LowLevelKeyboardListener();
+            listener.OnKeyPressed += KeyboardPressed;
+            listener.HookKeyboard();
+        }
+        private void mh_MouseDownEvent(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void mh_MouseUpEvent(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void mh_MouseMoveEvent(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void mh_MouseClickEvent(object sender, MouseEventArgs e)
+        {
+            if (MouseState)
+            {
+                if (clicked)
+                {
+                    clicked = false;
+                }
+                else
+                {
+                    clicked = true;
+                    //MessageBox.Show(e.X + "-" + e.Y);
+                    if (e.Button != MouseButtons.None)
+                    {
+
+                        MouseSound.Play();
+
+                    }
+
+                }
+            }
+        }
+
+        private void KeyboardPressed(object sender, KeyPressedArgs e)
+        {
+            if (KeyboardState)
+            {
+                if (KeyboardSoundState)
+                {
+                    KeyboardSoundState = false;
+                    KeyboardSound1.Play();
+                }
+                else
+                {
+                    KeyboardSoundState = true;
+                    KeyboardSound2.Play();
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            mh.UnHook();
+            listener.UnHookKeyboard();
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MouseState)
+            {
+                MouseState = false;
+                button1.BackgroundImage = Properties.Resources.off;
+            }
+            else
+            {
+                MouseState = true;
+                button1.BackgroundImage = Properties.Resources.on;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (KeyboardState)
+            {
+                KeyboardState = false;
+                button2.BackgroundImage = Properties.Resources.off;
+            }
+            else
+            {
+                KeyboardState = true;
+                button2.BackgroundImage = Properties.Resources.on;
+            }
+        }
+    }
+}
+#region kod ze strony: https://github.com/kellman616/TouchScreenControlEverythingDemo/blob/master/Demo1_Detect_Mouse_Position_And_Click_Event/Demo_mousehook_csdn/Form1.cs
+
+public class Win32Api
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public class POINT
+    {
+        public int x;
+        public int y;
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    public class MouseHookStruct
+    {
+        public POINT pt;
+        public int hwnd;
+        public int wHitTestCode;
+        public int dwExtraInfo;
+    }
+    public delegate int HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+    [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+    public static extern int SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hInstance, int threadId);
+    [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+    public static extern bool UnhookWindowsHookEx(int idHook);
+    [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+    public static extern int CallNextHookEx(int idHook, int nCode, IntPtr wParam, IntPtr lParam);
+}
+
+
+public class MouseHook
+{
+    private Point point;
+    private Point Point
+    {
+        get { return point; }
+        set
+        {
+            if (point != value)
+            {
+                point = value;
+                if (MouseMoveEvent != null)
+                {
+                    var e = new MouseEventArgs(MouseButtons.None, 0, point.X, point.Y, 0);
+                    MouseMoveEvent(this, e);
+                }
+            }
+        }
+    }
+    private int hHook;
+    private const int WM_MOUSEMOVE = 0x200;
+    private const int WM_LBUTTONDOWN = 0x201;
+    private const int WM_RBUTTONDOWN = 0x204;
+    private const int WM_MBUTTONDOWN = 0x207;
+    private const int WM_LBUTTONUP = 0x202;
+    private const int WM_RBUTTONUP = 0x205;
+    private const int WM_MBUTTONUP = 0x208;
+    private const int WM_LBUTTONDBLCLK = 0x203;
+    private const int WM_RBUTTONDBLCLK = 0x206;
+    private const int WM_MBUTTONDBLCLK = 0x209;
+    public const int WH_MOUSE_LL = 14;
+    public Win32Api.HookProc hProc;
+    public MouseHook()
+    {
+        this.Point = new Point();
+    }
+    public int SetHook()
+    {
+        hProc = new Win32Api.HookProc(MouseHookProc);
+        hHook = Win32Api.SetWindowsHookEx(WH_MOUSE_LL, hProc, IntPtr.Zero, 0);
+        return hHook;
+    }
+    public void UnHook()
+    {
+        Win32Api.UnhookWindowsHookEx(hHook);
+    }
+    private int MouseHookProc(int nCode, IntPtr wParam, IntPtr lParam)
+    {
+        Win32Api.MouseHookStruct MyMouseHookStruct = (Win32Api.MouseHookStruct)Marshal.PtrToStructure(lParam, typeof(Win32Api.MouseHookStruct));
+        if (nCode < 0)
+        {
+            return Win32Api.CallNextHookEx(hHook, nCode, wParam, lParam);
+        }
+        else
+        {
+            if (MouseClickEvent != null)
+            {
+                MouseButtons button = MouseButtons.None;
+                int clickCount = 0;
+                switch ((Int32)wParam)
+                {
+                    case WM_LBUTTONDOWN:
+                        button = MouseButtons.Left;
+                        clickCount = 1;
+                        MouseDownEvent(this, new MouseEventArgs(button, clickCount, point.X, point.Y, 0));
+                        break;
+                    case WM_RBUTTONDOWN:
+                        button = MouseButtons.Right;
+                        clickCount = 1;
+                        MouseDownEvent(this, new MouseEventArgs(button, clickCount, point.X, point.Y, 0));
+                        break;
+                    case WM_MBUTTONDOWN:
+                        button = MouseButtons.Middle;
+                        clickCount = 1;
+                        MouseDownEvent(this, new MouseEventArgs(button, clickCount, point.X, point.Y, 0));
+                        break;
+                    case WM_LBUTTONUP:
+                        button = MouseButtons.Left;
+                        clickCount = 1;
+                        MouseUpEvent(this, new MouseEventArgs(button, clickCount, point.X, point.Y, 0));
+                        break;
+                    case WM_RBUTTONUP:
+                        button = MouseButtons.Right;
+                        clickCount = 1;
+                        MouseUpEvent(this, new MouseEventArgs(button, clickCount, point.X, point.Y, 0));
+                        break;
+                    case WM_MBUTTONUP:
+                        button = MouseButtons.Middle;
+                        clickCount = 1;
+                        MouseUpEvent(this, new MouseEventArgs(button, clickCount, point.X, point.Y, 0));
+                        break;
+                }
+
+                var e = new MouseEventArgs(button, clickCount, point.X, point.Y, 0);
+                MouseClickEvent(this, e);
+            }
+            this.Point = new Point(MyMouseHookStruct.pt.x, MyMouseHookStruct.pt.y);
+            return Win32Api.CallNextHookEx(hHook, nCode, wParam, lParam);
+        }
+    }
+
+    public delegate void MouseMoveHandler(object sender, MouseEventArgs e);
+    public event MouseMoveHandler MouseMoveEvent;
+
+    public delegate void MouseClickHandler(object sender, MouseEventArgs e);
+    public event MouseClickHandler MouseClickEvent;
+
+    public delegate void MouseDownHandler(object sender, MouseEventArgs e);
+    public event MouseDownHandler MouseDownEvent;
+
+    public delegate void MouseUpHandler(object sender, MouseEventArgs e);
+    public event MouseUpHandler MouseUpEvent;
+
+
+}
+
+#endregion
+
+
+#region kod ze strony: http://www.dylansweb.com/2014/10/low-level-global-keyboard-hook-sink-in-c-net/
+public class LowLevelKeyboardListener
+{
+    private const int WH_KEYBOARD_LL = 13;
+    private const int WM_KEYDOWN = 0x0100;
+    private const int WM_SYSKEYDOWN = 0x0104;
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+    public event EventHandler<KeyPressedArgs> OnKeyPressed;
+
+    private LowLevelKeyboardProc _proc;
+    private IntPtr _hookID = IntPtr.Zero;
+
+    public LowLevelKeyboardListener()
+    {
+        _proc = HookCallback;
+    }
+
+    public void HookKeyboard()
+    {
+        _hookID = SetHook(_proc);
+    }
+
+    public void UnHookKeyboard()
+    {
+        UnhookWindowsHookEx(_hookID);
+    }
+
+    private IntPtr SetHook(LowLevelKeyboardProc proc)
+    {
+        using (Process curProcess = Process.GetCurrentProcess())
+        using (ProcessModule curModule = curProcess.MainModule)
+        {
+            return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+        }
+    }
+
+    private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+    {
+        if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN)
+        {
+            int vkCode = Marshal.ReadInt32(lParam);
+
+            if (OnKeyPressed != null) { OnKeyPressed(this, new KeyPressedArgs(KeyInterop.KeyFromVirtualKey(vkCode))); }
+        }
+
+        return CallNextHookEx(_hookID, nCode, wParam, lParam);
+    }
+}
+
+public class KeyPressedArgs : EventArgs
+{
+    public Key KeyPressed { get; private set; }
+
+    public KeyPressedArgs(Key key)
+    {
+        KeyPressed = key;
+    }
+}
+#endregion
